d2dd26208fdcae1c370f0252a9fba56cd69b3e97
Sumuditha Lansakara <sumudithalanz@gmail.com>
2024-03-02 20:32:27 +0530

    basic chat app in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 const App = () => {
+  const [error, setError] = useState("");
+
   return (
     <div className="app">
       <section className="search-section">
@@ -6,6 +10,17 @@ const App = () => {
           What do you want to know?
           <button className="surprise">Surprise me</button>
         </p>
+        <div className="input-container">
+          <input value={""} placeholder="When is Christmas...?" onChange={""} />
+          {!error && <button>Ask me</button>}
+          {error && <button>Clear</button>}
+        </div>
+        {error && <p className="error">{error}</p>}
+        <div className="search-result">
+          <div key={""}>
+            <p className="answer"></p>
+          </div>
+        </div>
       </section>
     </div>
   );
