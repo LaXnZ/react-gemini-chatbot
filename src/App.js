bfded41ba555fd0b584382b004b28843790e439c
Sumuditha Lansakara <sumudithalanz@gmail.com>
2024-03-02 20:20:09 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <section className="search-section">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          What do you want to know?
+          <button className="surprise">Surprise me</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
